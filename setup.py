90f6b34201bae015d98b4019ce824ea694b12d98
Adam Vignolles <adam.vignolles@gmail.com>
2022-06-05 15:37:46 +0200

    Start project and do the setup file with his requirements

---
+++ setup.py
@@ -0,0 +1,13 @@
+"""setup
+
+    This is the setup file for the My_Game package.
+"""
+import os # Import the os module
+
+def main():
+    """
+    This is the main function for the setup file.
+    """
+    os.system('pip install -r requirements.txt')
+
+main()  # Call the main function
